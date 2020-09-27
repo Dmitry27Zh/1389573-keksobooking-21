7ae254a81f728c09f1c723bcdd6924546d477bdf
Dmitry <dmitry27zh@gmail.com>
2020-09-27 17:54:04 +0300

    созданы и добавлены DOM элементы - пины

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const getNewArray = function (array) {
 };
 
 const Author = function (x) {
-  this.avatar = `img/avatars/user0${x}`;
+  this.avatar = `img/avatars/user0${x}.png`;
 };
 
 const Offer = function () {
@@ -60,3 +60,51 @@ const generateAds = function () {
 };
 
 document.querySelector(`.map`).classList.remove(`map--faded`);
+const createPopup = function (ad) {
+  const template = document.querySelector(`#card`).content.querySelector(`.popup`);
+  const mapPopup = template.cloneNode(true);
+  mapPopup.querySelector(`.popup__title`).textContent = ad.offer.title;
+  mapPopup.querySelector(`.popup__text--address`).textContent = ad.offer.address;
+  mapPopup.querySelector(`.popup__text--price`).textContent = ad.offer.price;
+  mapPopup.querySelector(`.popup__type`).textContent = ad.offer.type;
+  mapPopup.querySelector(`.popup__text--capacity`).textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  mapPopup.querySelector(`.popup__text--time`).textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
+  const features = mapPopup.querySelectorAll(`.popup__feature`);
+  for (let feature of features) {
+    feature.classList.add(`hidden`);
+  }
+  ad.offer.features.forEach(function (feature) {
+    mapPopup.querySelector(`.popup__feature--${feature}`).classList.remove(`hidden`);
+  });
+  mapPopup.querySelector(`.popup__description`).textContent = ad.offer.description;
+  const popupPhotos = mapPopup.querySelector(`.popup__photos`);
+  const popupPhoto = popupPhotos.querySelector(`.popup__photo`);
+  popupPhotos.removeChild(popupPhoto);
+  ad.offer.photos.forEach(function (photo) {
+    let popupPhotoCopy = popupPhoto.cloneNode(true);
+    popupPhotoCopy.src = photo;
+    popupPhotos.appendChild(popupPhotoCopy);
+  });
+  return mapPopup;
+};
+
+const createPin = function (ad) {
+  const template = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+  const mapPin = template.cloneNode(true);
+  mapPin.style = `left: ${ad.location.x}px; top: ${ad.location.y}px;`;
+  mapPin.children[0].src = ad.author.avatar;
+  mapPin.children[0].alt = ad.offer.title;
+  return mapPin;
+};
+
+const addPins = function (array) {
+  let fragment = document.createDocumentFragment();
+  array.forEach(function (item) {
+    fragment.appendChild(createPin(item));
+  });
+  return fragment;
+};
+
+createPopup(ads[0]);
+let ads = generateAds();
+document.querySelector(`.map__pins`).appendChild(addPins(ads));
