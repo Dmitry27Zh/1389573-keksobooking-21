80d5af4b0647e9091b12303a5e54d730cd640925
Dmitry <dmitry27zh@gmail.com>
2020-10-16 13:19:45 +0300

    правка открытия карточек

--- js/map.js
+++ js/map.js
@@ -7,44 +7,53 @@
   const mapFiltersList = mapFilters.querySelectorAll(`.map__filter`);
   const mapFeaturesFieldset = mapFilters.querySelector(`.map__features`);
 
-  let createdPopup;
-  let popupClose;
-  let currentTarget;
+  const activateAdsViewing = function () {
+    let createdPopup;
+    let popupClose;
+    let currentTarget;
 
-  const openCard = function (evt) {
-    currentTarget = evt.currentTarget;
-    currentTarget.removeEventListener(`click`, mapPinClickHandler);
-    currentTarget.classList.add(`.map__pin--active`);
-    const index = Array.from(mapPinsList).indexOf(currentTarget);
-    createdPopup = window.card.createPopup(window.data.ads[index]);
-    map.insertBefore(createdPopup, mapFiltersContainer);
-    popupClose = createdPopup.querySelector(`.popup__close`);
-    popupClose.addEventListener(`click`, popupCloseClickHadnler);
-    document.addEventListener(`keydown`, popupKeydownHandler);
-  };
+    const openCard = function (evt) {
+      currentTarget = evt.currentTarget;
+      currentTarget.removeEventListener(`click`, mapPinClickHandler);
+      currentTarget.classList.add(`.map__pin--active`);
+      const index = Array.from(mapPinsList).indexOf(currentTarget);
+      createdPopup = window.card.createPopup(window.data.ads[index]);
+      map.insertBefore(createdPopup, mapFiltersContainer);
+      popupClose = createdPopup.querySelector(`.popup__close`);
+      popupClose.addEventListener(`click`, popupCloseClickHadnler);
+      document.addEventListener(`keydown`, popupKeydownHandler);
+    };
 
-  const closeCard = function () {
-    currentTarget.classList.remove(`.map__pin--active`);
-    currentTarget.addEventListener(`click`, mapPinClickHandler);
-    map.removeChild(createdPopup);
-    createdPopup = undefined;
-    popupClose.removeEventListener(`click`, popupCloseClickHadnler);
-    document.removeEventListener(`keydown`, popupKeydownHandler);
-  };
+    const closeCard = function () {
+      currentTarget.classList.remove(`.map__pin--active`);
+      currentTarget.addEventListener(`click`, mapPinClickHandler);
+      map.removeChild(createdPopup);
+      createdPopup = undefined;
+      popupClose.removeEventListener(`click`, popupCloseClickHadnler);
+      document.removeEventListener(`keydown`, popupKeydownHandler);
+    };
 
-  const popupCloseClickHadnler = function () {
-    closeCard();
-  };
-
-  const popupKeydownHandler = function (evt) {
-    window.utils.isEscEvent(evt, closeCard);
-  };
-
-  const mapPinClickHandler = function (evt) {
-    if (createdPopup) {
+    const popupCloseClickHadnler = function () {
       closeCard();
+    };
+
+    const popupKeydownHandler = function (evt) {
+      window.utils.isEscEvent(evt, closeCard);
+    };
+
+    const mapPinClickHandler = function (evt) {
+      if (createdPopup) {
+        closeCard();
+      }
+      openCard(evt);
+    };
+
+    window.pin.addPins();
+    const mapPinsList = map.querySelectorAll(`.map__pin:not(.map__pin--main)`);
+
+    for (let i = 0; i < mapPinsList.length; i++) {
+      mapPinsList[i].addEventListener(`click`, mapPinClickHandler);
     }
-    openCard(evt);
   };
 
   const disableMap = function () {
@@ -54,19 +63,13 @@
     mapFeaturesFieldset.setAttribute(`disabled`, `true`);
   };
 
-  let mapPinsList;
-
   const enableMap = function () {
     map.classList.remove(`map--faded`);
     mapFiltersList.forEach(function (filter) {
       filter.removeAttribute(`disabled`);
     });
     mapFeaturesFieldset.removeAttribute(`disabled`);
-    window.pin.addPins();
-    mapPinsList = map.querySelectorAll(`.map__pin:not(.map__pin--main)`);
-    for (let i = 0; i < mapPinsList.length; i++) {
-      mapPinsList[i].addEventListener(`click`, mapPinClickHandler);
-    }
+    activateAdsViewing();
   };
 
   window.map = {
