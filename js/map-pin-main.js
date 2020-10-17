4e41a60fb54770394f161f6157d82478de6848be
Dmitry <dmitry27zh@gmail.com>
2020-10-17 11:16:49 +0300

    начало перемещения

--- js/map-pin-main.js
+++ js/map-pin-main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function () {
+  const mapPins = window.pin.mapPins;
   const mapPinMain = window.pin.mapPins.querySelector(`.map__pin--main`);
   const mapPinMainImg = mapPinMain.querySelector(`img`);
 
@@ -22,12 +23,44 @@
     return Math.round(mapPinMain.offsetTop + MapPinMainSizes.Height);
   };
 
+  const moveMapPinMain = function (evt) {
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY,
+    };
+
+    const onMousemove = function (moveEvt) {
+      const move = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY,
+      };
+
+      mapPinMain.style.left = `${mapPinMain.offsetLeft - move.x}px`;
+      mapPinMain.style.top = `${mapPinMain.offsetTop - move.y}px`;
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY,
+      };
+    };
+
+    const onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      mapPins.removeEventListener(`mousemove`, onMousemove);
+      mapPins.removeEventListener(`mouseup`, onMouseUp);
+    };
+
+    mapPins.addEventListener(`mousemove`, onMousemove);
+    mapPins.addEventListener(`mouseup`, onMouseUp);
+  };
+
   const mapPinMainMousedownHandler = function (evt) {
     window.utils.isMousedownEvent(evt, function () {
       if (window.map.element.classList.contains(`map--faded`)) {
         window.main.activateElements();
         mapPinMain.removeEventListener(`keydown`, mapPinMainKeydownHandler);
       }
+      moveMapPinMain(evt);
     });
   };
 
