bd422fb71a299073f599b50979f5b4a4a46cd1f5
Dmitry <dmitry27zh@gmail.com>
2020-10-19 21:52:49 +0300

    добавлен запрос на получение данных

---
+++ js/backend.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+  const URL_LOAD = `https://21.javascript.pages.academy/keksobooking/data`;
+
+  const load = function (onSuccess, onError, index) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(`get`, URL_LOAD);
+    xhr.responseType = `json`;
+    xhr.send();
+    xhr.addEventListener(`load`, function () {
+      if (index) {
+        onSuccess(xhr.response[index]);
+      } else {
+        onSuccess(xhr.response);
+      }
+    });
+    xhr.addEventListener(`error`, function () {
+      onError(`Ошибка подключения к сети`);
+    });
+  };
+
+  window.backend = {
+    load,
+  };
+})();
